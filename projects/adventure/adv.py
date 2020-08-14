069203433241108c23354ab05893bd3bff260b6c
arinramer <arinramer@gmail.com>
2020-08-13 20:58:47 -0700

    Done

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -12,9 +12,9 @@ world = World()
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
-map_file = "maps/test_loop.txt"
+# map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -25,56 +25,34 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
-def bfs(direction):
-    q = Queue()
-    q.enqueue([direction])
-
-    while q.size() > 0:
-        current_path = q.dequeue()
-        node = current_path[-1]
-        player.travel(node)
-        directions = player.current_room.get_exits()
-        for direction in directions:
-            if graph.get(player.current_room.id) is not None:
-                if graph[player.current_room.id][direction] == '?':
-                    for i in current_path:
-                        traversal_path.append(i)
-            else:
-                new_path = list(current_path)
-                new_path.append(direction)
-                q.enqueue(new_path)
-
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
-s = []
-graph = {
-  0: {'n': '?', 's': '?', 'w': '?', 'e': '?'}
-}
-directions = player.current_room.get_exits()
-for direction in directions:
-    if graph[player.current_room.id][direction] == '?':
-        s.append(direction)
 
-while len(s) > 0:
-    current_direction = s.pop()
-    prev = player.current_room.id
-    directions = player.current_room.get_exits()
-    if current_direction in directions:
-        player.travel(current_direction)
-    graph[prev][current_direction] = player.current_room.id
-    
-    for direction in directions:
-        if player.current_room.id not in graph:
-            graph[player.current_room.id] = {}
-        if direction not in graph[player.current_room.id]:
-            graph[player.current_room.id][direction] = '?'                
-        if graph[player.current_room.id][direction] == '?':
-            s.append(direction)
-            traversal_path.append(direction)
-    # if '?' not in graph[player.current_room.id]:
-    #     bfs(direction)
-print(graph)
+backtrack = []
+reversed_directions = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+
+visited = set()
+
+while len(visited) < len(room_graph):
+    next_move = None
+    for exit in player.current_room.get_exits():
+        if player.current_room.get_room_in_direction(exit) not in visited:
+            next_move = exit
+            break
+
+    if next_move is not None:
+        traversal_path.append(next_move)
+        backtrack.append(reversed_directions[next_move])
+        player.travel(next_move)  
+        visited.add(player.current_room)
+
+    else:
+        next_move = backtrack.pop()
+        traversal_path.append(next_move)
+        player.travel(next_move)
+
+
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
